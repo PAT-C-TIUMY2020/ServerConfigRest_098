b12241894ce8321e165c287178a7e19c40b710fe
Nashrul Saefullah <nashrulsaefullah@gmail.com>
2020-12-20 21:30:50 +0700

    Add project files.

---
+++ ServerConfigRest_098_Nashrul/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_098_Nashrul;
+
+
+namespace ServerConfigRest_098_Nashrul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server ready . .");
+                Console.ReadLine();
+                hostObjek.Close();
+
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+    }
+
+
+
+}
